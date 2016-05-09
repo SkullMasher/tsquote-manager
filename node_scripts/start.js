e947b2c51646917d99c476a22949fe44681cde2f
skullmasher <skullmasher@heartlessgaming.com>
2016-05-09 20:29:09 +0200

    [WIP] Browser-sync reaload problem

--- node_scripts/start.js
+++ node_scripts/start.js
@@ -1,14 +1,16 @@
-var fs = require('fs')
-var chalk = require('chalk')
-var dateFormat = require('dateformat')
-var sass = require('node-sass')
-var bs = require('browser-sync').create()
+'use strict'
+
+let fs = require('fs')
+let chalk = require('chalk')
+let dateFormat = require('dateformat')
+let sass = require('node-sass')
+let bs = require('browser-sync').create()
 
 // console.log for 1337 h4X0r
-var log = console.log.bind(console)
+let log = console.log.bind(console)
 
 // ISO date format to use for debuging
-// var now = new Date()
+// let now = new Date()
 
 // Greeting Message
 log(chalk.red('  #####   '))
@@ -20,43 +22,45 @@ log(chalk.red(' ### ###  '))
 log(chalk.red('  #####   '))
 log(chalk.red('  # # #   ') + chalk.grey(' Play more, care less, be an heartless'))
 
+let appFolderPath = 'app'
+
 // Reload all browser on HTML change
-bs.watch('*.html').on('change', function () {
+bs.watch(appFolderPath + '/' + '*.html').on('change', function () {
   bs.notify("<span color='green'>HTML Reloaded</span>", 2000)
   bs.reload()
 })
 
 // Reload all browser on JS change
-bs.watch('js/**.js').on('change', function () {
+bs.watch(appFolderPath + '/' + 'js/**.js').on('change', function () {
   bs.notify("<span color='green'>JS Reloaded</span>", 2000)
   bs.reload()
 })
 
 // Specific compilation for SASS file
-bs.watch('sass/**.scss', function (event, file) {
+bs.watch(appFolderPath + '/' + 'sass/**.scss', function (event, file) {
   if (event === 'change') {
     sass.render({
-      file: 'sass/style.scss',
+      file: appFolderPath + '/' + 'sass/style.scss',
       outputStyle: 'expanded',
-      outFile: 'css/style.css',
+      outFile: appFolderPath + '/' + 'css/style.css',
       sourceMap: true
     }, function (error, result) {
       if (error) {
         // Pretty Debug Message on sass error
 
-        var nowFormat = dateFormat(new Date(), 'HH:MM:ss')
+        let nowFormat = dateFormat(new Date(), 'HH:MM:ss')
 
         log(chalk.red('[SASS ERROR ' + nowFormat + '] ') + error.file)
         log(chalk.red('[SASS ERROR ' + nowFormat + '] ') + 'On line ' + chalk.red(error.line) + ' at column ' + chalk.red(error.column))
         log(chalk.red('[SASS ERROR ' + nowFormat + '] ') + error.message)
       } else {
         // Creating css style files
-        fs.writeFile('css/style.css', result.css, function (err) {
+        fs.writeFile(appFolderPath + '/' + 'css/style.css', result.css, function (err) {
           if (!err) {
             // Creating css map file
-            fs.writeFile('css/style.map.css', result.map, function (err) {
+            fs.writeFile(appFolderPath + '/' + 'css/style.map.css', result.map, function (err) {
               if (!err) {
-                var nowFormat = dateFormat(new Date(), '[HH:MM:ss]')
+                let nowFormat = dateFormat(new Date(), '[HH:MM:ss]')
                 log(nowFormat + chalk.green(' CSS Reloaded'))
                 bs.notify("<span color='green'>CSS Reloaded</span>", 2000)
               } else {
@@ -68,7 +72,7 @@ bs.watch('sass/**.scss', function (event, file) {
           }
         })
         // Injecting the CSS change in BrowserSync
-        bs.reload('css/style.css')
+        bs.reload(appFolderPath + '/' + 'css/style.css')
       }
     })
   }
@@ -76,5 +80,5 @@ bs.watch('sass/**.scss', function (event, file) {
 
 // Now init the Browsersync server
 bs.init({
-  server: './'
+  server: './' + appFolderPath + '/'
 })
